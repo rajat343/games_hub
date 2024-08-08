2a73db586b5c1230c393b4eeebc8c48de1405430
Rajat Mishra <rajatmishra343@gmail.com>
2024-08-09 03:35:57 +0530

    dark mode functionality added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
 import logo from '../assets/games_hub.jpg'
+import ColourModeSwitch from './ToggleColourModeSwitch'
 
 const NavBar = () => {
     return (
         <HStack>
             <Image src = {logo} boxSize='60px'/>
-            <Text>Navbar</Text>
+            <ColourModeSwitch />
         </HStack>
     )
 }
---
+++ src/components/ToggleColourModeSwitch.tsx
@@ -0,0 +1,13 @@
+import { Button, HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ColourModeSwitch = () => {
+    const { toggleColorMode, colorMode } = useColorMode();
+    return (
+      <HStack>
+		<Switch colorScheme="green" isChecked = {colorMode === "dark"} onChange={toggleColorMode}></Switch>
+		<Text>Dark Mode</Text>
+	  </HStack>
+    )
+}
+
+export default ColourModeSwitch
